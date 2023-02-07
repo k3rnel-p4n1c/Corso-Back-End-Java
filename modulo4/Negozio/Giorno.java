964733ca2602bf30919d1b1d7bbb8b57ba79591c
k3rnel-p4n1c <123565342+k3rnel-p4n1c@users.noreply.github.com>
2023-02-07 20:41:39 +0100

    Delete modulo4/Negozio directory
    Nome package con lettera maiuscola- cancello per poi ricaricare

--- modulo4/Negozio/Giorno.java
+++
@@ -1,8 +0,0 @@
-package modulo4.negozio;
-
-/**
- * Giorni della settimana. In base a questi giorni, il negozio offre uno sconto del 20% sui prodotti alimentari il lun e il merc
- */
-enum Giorno {
-    LUNEDI, MARTEDI, MERCOLEDI, GIOVEDI, VENERDI, SABATO, DOMENICA
-}
